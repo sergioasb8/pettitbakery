fd903381a18f4edc2deebf863996cdbcc1cda0b7
sergioasb8 <sergioasb8@gmail.com>
2021-03-13 15:32:10 -0500

    add

--- src/components/pieces/CategoryCircle.js
+++ src/components/pieces/CategoryCircle.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import '../../assets/styles/Categories.css';
 
 export const CategoryCircle = ({name,id,active,handleActive,Image}) => {
   return (
--- src/routers/AuthRouter.js
+++ src/routers/AuthRouter.js
@@ -2,7 +2,7 @@ import React from 'react'
 import { Redirect,Switch, Route } from 'react-router-dom'
 import Cart from '../components/auth/Cart';
 import Home from '../components/auth/Home';
-// import ProductView from '../components/auth/ProductView';
+import ProductView from '../components/auth/ProductView';
 
 export const AuthRouter = () => {
     return (
@@ -10,7 +10,7 @@ export const AuthRouter = () => {
             <Switch>
                 <Route exact path="/auth/home" component = { Home } />
                 <Route exact path="/auth/cart" component = { Cart } />
-                {/* <Route exact path="/auth/product" component = { ProductView } /> */}
+                <Route exact path="/auth/product" component = { ProductView } />
                 <Redirect to="/" /> 
             </Switch>
         </>
