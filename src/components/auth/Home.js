37b77c874435ef7731d39bc3b275e94c5c201623
sergioasb8 <sergioasb8@gmail.com>
2021-03-13 19:41:11 -0500

    add fix

--- src/components/auth/Home.js
+++ src/components/auth/Home.js
@@ -41,8 +41,6 @@ function Home () {
                         <Product
                             key = {item.id}
                             item = {item}
-                            onAdd = {onAdd}
-                            onRemove = {onRemove}
                         />
                     ))
                 }
