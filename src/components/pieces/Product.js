f37f702f76d0b520ed3d6af8f94c4086af53c2de
sergioasb8 <sergioasb8@gmail.com>
2021-03-13 23:31:15 -0500

    container price

--- src/components/pieces/Product.js
+++ src/components/pieces/Product.js
@@ -10,7 +10,13 @@ export const Product = ({nameProduct, imgPro}) => {
             </div>
             <div className='ProductImage' >
                 <img src={imgPro} alt={nameProduct} className="CakeImage" />
-            </div>                
+            </div>    
+            <div className="buttonsContainer">
+                <div className="divText Price">30000</div>
+                <button className="buttonPrice Minus">-</button>
+                <div className="divText Amount">0</div>
+                <button className="buttonPrice Plus">+</button>
+            </div>            
         </div>
     )
 }
