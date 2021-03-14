6cf25eef62dd18e8f768e375cc78f02aedfa7335
sergioasb8 <sergioasb8@gmail.com>
2021-03-14 11:29:21 -0500

    cart products

--- src/components/auth/Cart.js
+++ src/components/auth/Cart.js
@@ -1,21 +1,33 @@
 import React, { useContext } from 'react'
-import '../../assets/styles/Home.css';
+import '../../assets/styles/Cart.css';
 import { ProductContainer } from '../../containers/ProductContainer';
 import { Footer } from '../pieces/Footer';
 import { Header } from '../pieces/Header';
 import { faArrowAltCircleLeft } from '@fortawesome/free-solid-svg-icons';
 import { Context } from '../context/Context'
+import { ProductCart } from '../pieces/ProductCart';
 
 function Cart () {
 
-    const {carItems} = useContext(Context)
+    const { onAdd, carItems, newProducts, onRemove} = useContext(Context)
 
     return (
         <div className='Cart'>
             <Header />
             <div className='CartContainer'>
+                <h1 className="OrderTitle">Orden</h1>
                 <ProductContainer>
-
+                    {
+                        carItems.map((item)=>(
+                            <ProductCart
+                                key = {item.id}
+                                item = {item}
+                                onAdd = {onAdd}
+                                onRemove = {onRemove}
+                                newProducts = {newProducts}
+                            />
+                        ))
+                    }
                 </ProductContainer>
             </div>
             <Footer Icons={faArrowAltCircleLeft} liknRoute="/auth/home" carItems={carItems}/>
---
+++ src/components/pieces/ProductCart.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import '../../assets/styles/ProductCart.css';
+
+export const ProductCart = ({item, onAdd, onRemove, newProducts}) => {
+
+    //Asign the correct format to price values
+    const formatter = new Intl.NumberFormat('es-CO',{
+        style: 'currency',
+        currency: 'COP',
+        minimumFractionDigits: 0
+    })
+    
+    const founded = newProducts.find(y => y.id === item.id);
+
+    const itemAmount = founded.amount;
+
+    return (
+        <div className="ProductContainer">
+            <div className='ProductNameContainer'>
+                    <h3 className='ProductName'>{item.nameProduct}</h3>
+            </div>
+            <div className='ProductImage' >
+                <img src={item.imgPro} alt={item.nameProduct} className="CakeImage" />
+            </div>    
+            <div className="buttonsContainer">
+                <div className="divText Price">{formatter.format(item.price)}</div>
+                <button className="buttonPrice Minus" onClick={() => onRemove(item)}>-</button>
+                <div className="divText Amount">{itemAmount}</div>
+                <button className="buttonPrice Plus" onClick={() => onAdd(item)}>+</button>
+            </div>            
+        </div>
+    )
+}
